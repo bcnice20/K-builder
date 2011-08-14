b07146106ab9930516837cfe2bb3f48391955d67
brad <bcnice20@gmail.com>
2011-08-14 03:52:13 -0300

    there we go (i think)

--- k-builder.rb
+++ k-builder.rb
@@ -3,34 +3,33 @@
 system('clear')
 
 def menu
-	puts "please choose one of the availble commands"
+  puts "please choose one of the availble commands"
 
-	menu = [
-		"1: make menuconfig",
-		"2: make defconfig",
-		"3: make clean",
-		"4: build kernel",
-		"5: move modules and zImage",
-		"6: make any-kernel zip",
-		"7: build boot.img",
-		"8: build flashable boot.img zip",
-		"9: exit"
-	]
-
-	puts menu
+  menu = [
+    "1: make menuconfig",
+    "2: make defconfig",
+    "3: make clean",
+    "4: build kernel",
+    "5: move modules and zImage",
+    "6: make any-kernel zip",
+    "7: build boot.img",
+    "8: build flashable boot.img zip",
+    "9: exit"
+ ]
+ puts menu
 
 end
 
 def main
-	menu()
+  menu()
 
-	s = ""
-	while s != "9"
-		print "> "
-		s = gets.chomp
-		system('clear')
-		menu()
-	end
+  s = ""
+  while s != "9"
+    print "> "
+    s = gets.chomp
+    system('clear')
+    menu()
+  end
 
 end
 
